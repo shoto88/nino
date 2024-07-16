a18525887852122c863616569d9a64a6bf55a4af
shoto88 <shotoharu@icloud.com>
2024-07-16 22:27:40 +0900

    feat(page):記事一覧画面追加fix #2

---
+++ app/posts/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { getPosts } from "./data";
+
+export default async function Home() {
+  const posts = await getPosts();
+  return (
+<div>
+  <h1>記事一覧</h1>
+  {posts.map((post) => (
+    <div key={post.id}>
+
+      <h2>
+        <Link href={`/posts/${post.id}`}>{post.title.rendered}</Link>
+      </h2>
+      <p>{post.id}</p>
+    </div>
+  ))}
+</div>
+  );
+}
