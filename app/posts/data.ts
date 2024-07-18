513533d9d8d33d7f47c03d7309fe249a5c9e1082
shoto88 <shotoharu@icloud.com>
2024-07-18 21:38:49 +0900

    fe

--- app/posts/data.ts
+++ app/posts/data.ts
@@ -3,7 +3,13 @@ import { POST_ENDPOINT_URL } from '@/lib/wp';
 import { Post } from '@/types/post';
 
 
-export const getPosts = async () => {
-    const res = await fetch(POST_ENDPOINT_URL);
+export const getPosts = async (page:number =1) => {
+  const res = await fetch(POST_ENDPOINT_URL + `?page=${page}&per_page=1`, {
+    cache: 'no-store',
+  });
+
+  const totalPageCount = res.headers.get('X-WP-TotalPages');
+
+
   return (await res.json()) as Post[];
 };
